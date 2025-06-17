8b8448725fa923afdcede2ede989f4cbd7d13819
mfc <2045425417@qq.com>
2025-06-17 15:12:54 +0800

    Init

---
+++ src/ProblemHandling.Exception/MfcException.cs
@@ -0,0 +1,13 @@
+﻿using System.Net;
+
+namespace MakimaFC.Extensions.WebApi.ProblemHandling.Exception;
+
+public class MfcException : System.Exception
+{
+    public HttpStatusCode StatusCode { get; init; }
+
+    public MfcException(HttpStatusCode statusCode, string message) : base(message)
+    {
+        StatusCode = statusCode;
+    }
+}
---
+++ src/ProblemHandling/MfcExceptionFilter.cs
@@ -0,0 +1,19 @@
+﻿using MakimaFC.Extensions.WebApi.ProblemHandling.Exception;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace MakimaFC.Extensions.WebApi.ProblemHandling;
+
+public class MfcExceptionFilter : IAsyncExceptionFilter
+{
+    public async Task OnExceptionAsync(ExceptionContext context)
+    {
+        if (context.Exception is MfcException mfcException)
+        {
+            context.HttpContext.Response.StatusCode = (int)mfcException.StatusCode;
+            context.HttpContext.Response.ContentType = "application/json";
+            await context.HttpContext.Response.WriteAsync(mfcException.Message);
+            context.ExceptionHandled = true;
+        }
+    }
+}
